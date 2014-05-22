31522394ed19f17f008134c0a85034d18119e4de
mao <myj226@gmail.com>
2014-05-23 00:12:18 +0800

    update

--- javascripts/products.js
+++ javascripts/products.js
@@ -1,1 +1,36 @@
+var $, productsMap, tag;
 
+$ = jQuery;
+
+String.prototype.format = function() {
+  var i, s;
+  s = this;
+  i = arguments.length;
+  while (i--) {
+    s = s.replace(new RegExp("\\{" + i + "\\}", "gm"), arguments[i]);
+  }
+  return s;
+};
+
+productsMap = {
+  item1: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'],
+  item2: ['6.jpg', '7.jpg', '8.jpg']
+};
+
+tag = '<div class="items"><img class="img-rounded img-responsive" src="/images/danzhi/{0}" alt="Responsive image"></div>';
+
+$('.sidebar li').each(function(i, el) {
+  var cl;
+  cl = $(el).attr('class');
+  console.log(cl);
+  return $(el).on('click', function() {
+    var _results;
+    $('.contents').empty();
+    _results = [];
+    while (i < productsMap[cl].length) {
+      $('.contents').append(tag.format(productsMap[cl][i]));
+      _results.push(i++);
+    }
+    return _results;
+  });
+});
