149fe6cf815611f27ad25b6852aee3cc5346d92a
mao <myj226@gmail.com>
2014-06-02 12:43:05 +0800

    device css

--- assets/coffee/index.coffee
+++ assets/coffee/index.coffee
@@ -6,6 +6,6 @@ $(document).ready ->
     speed: 500
     autoplay: true
     arrows: false
-  if !Modernizr.backgroundsize
-    $('.navbar-brand').css 
-      background: "url('/images/sheash-logo-s.png') no-repeat"
+  # if !Modernizr.backgroundsize
+  #   $('.navbar-brand').css 
+  #     background: "url('/images/sheash-logo-s.png') no-repeat"
--- javascripts/index.js
+++ javascripts/index.js
@@ -3,15 +3,10 @@ var $;
 $ = jQuery;
 
 $(document).ready(function() {
-  $('.slider').slick({
+  return $('.slider').slick({
     dots: true,
     speed: 500,
     autoplay: true,
     arrows: false
   });
-  if (!Modernizr.backgroundsize) {
-    return $('.navbar-brand').css({
-      background: "url('/images/sheash-logo-s.png') no-repeat"
-    });
-  }
 });
