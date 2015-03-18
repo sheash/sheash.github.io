b4d2cfee87271786786128dd210230f4a6b2b9f4
mao <myj226@gmail.com>
2015-03-18 22:09:32 +0800

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var paths = {
 
 gulp.task('scss', function() {
   return gulp.src(paths.css)
-    .pipe(sass({ style: 'expanded' }))
+    .pipe(sass({ style: 'expanded', sourcemap: 'none' }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('stylesheets'))
 });
