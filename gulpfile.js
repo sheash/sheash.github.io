cea63df5932734dd102f3aa4e5f8952c20b349e0
mao <myj226@gmail.com>
2015-12-26 18:49:22 +0800

    finish new version

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,13 @@
 var gulp = require('gulp');
 
-var coffee = require('gulp-coffee');
-var sass = require('gulp-ruby-sass');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var autoprefixer = require('gulp-autoprefixer');
-var jade = require('gulp-jade');
 
 var paths = {
   scripts: ['assets/coffee/*.coffee'],
     css: ['assets/scss/*.scss']
 };
-
-gulp.task('scss', function() {
-  return sass('assets/scss/', { style: 'expanded', sourcemap: false })
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('stylesheets'))
-});
-
-gulp.task('templates', function() {
-  gulp.src('./*.jade')
-    .pipe(jade({
-        pretty: true
-    }))
-    .pipe(gulp.dest('./'))
-});
 
 gulp.task('scripts', function() {
   return gulp.src(paths.scripts)
@@ -41,9 +24,7 @@ gulp.task('scripts', function() {
 // });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['scripts']);
   gulp.watch(paths.css, ['scss']);
-  gulp.watch('./*.jade', ['templates']);
 });
 
-gulp.task('default', ['scripts', 'scss', 'templates', 'watch']);
+gulp.task('default', [ 'watch']);
